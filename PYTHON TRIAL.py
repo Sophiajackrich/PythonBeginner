dc5c8f16d313fe5a35aad543c9c6cb616d3b9608
Sophiajackrich <sophiajackrich75@gmail.com>
2024-09-16 11:08:26 +0100

    Modified commit

--- PYTHON TRIAL.py
+++
@@ -1,6 +0,0 @@
-print("A computer thought of a number from 1 - 10. Try to guess it.")
-secret_number = 7
-user_number = int(input("Enter your number: "))
-print(user_number)
-if secret_number > user_number:
-    print("Secret number greater than user number")
